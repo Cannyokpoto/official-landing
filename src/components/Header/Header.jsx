9cf4825cb6792668cd9009ffa224872af75066c6
unknown <promiseokpoto5050@gmail.com>
2025-05-02 12:40:11 +0100

    my last commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -31,7 +31,7 @@ function Header() {
       <nav className={`items-center justify-center list-none large:flex large:h-auto large:w-50 text-15px small:absolute small:top-0 small:left-0 bg-vogueWhite small:z-40 small:h-100vh small:border large:border-0 large:static small:pl-2 large:pl-0 small:hidden`}>
         
         <u className='flex justify-center h-auto gap-2 large:items-center w-100 large:flex-row small:flex-col small:items-start'>
-          <li><a href="https://pageinnovations.com.ng/" target='_blank' className='no-underline'>Home page</a></li>
+          <li><a href="https://pageinnovations.com.ng/" target='_blank' className='no-underline'>Home</a></li>
           <li><a href="https://pageinnovations.com.ng/why-choose-us/" target='_blank'>Why choose us</a></li>
           <li><a href="https://pageinnovations.com.ng/contact/" target='_blank'>Contact</a></li>
           <li><a href="https://pageinnovations.com.ng/academy/" target='_blank'>Academy</a></li>
